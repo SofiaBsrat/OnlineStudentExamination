ef0ab362b0f557b9b9cc7417301e7b5ae70dc12d
sofia bsrat <sbsrat@mum.edu>
2019-02-05 23:25:01 -0600

    worked on question management module

--- frontend/src/app/protected/admin/question-list/question-list.component.ts
+++ frontend/src/app/protected/admin/question-list/question-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { QuestionService } from 'src/app/services/question.service';
 
 @Component({
   selector: 'app-question-list',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionListComponent implements OnInit {
 
-  constructor() { }
+  questionForm: FormGroup;
+  questionList: any[];
+  isLoaded = false;
+  control: any;
+  questionSubscrption: Subscription;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, private questionService: QuestionService) { 
+    this.questionForm = formBuilder.group({
+      'category': ['', Validators.required],
+      'question': ['', Validators.required],
+      'active': ['', Validators.required]
+    });
+    this.control = this.questionForm.controls;
   }
 
+  ngOnInit() {
+    this.questionSubscrption = this.questionService.getQuestions().subscribe(data => {
+      if (data['status'] === 200) {
+        this.isLoaded = true;
+        this.questionList = data['data'];
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.questionSubscrption) {
+      this.questionSubscrption.unsubscribe();
+    }
+  }
+
+  onSubmit() {
+    const questionObj = {
+      category: this.control.category.value,
+      question: this.control.question.value,
+      active: this.control.active.value,
+    };
+
+    this.questionService.addQuestion(JSON.stringify(questionObj)).subscribe((response: any) => {
+      
+    });
+  }
 }
